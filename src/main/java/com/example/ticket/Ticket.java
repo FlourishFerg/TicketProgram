5422a6a347931c817f3dd7e27af70dc7a3d14224
Flourish <flourishferguson@gmail.com>
2024-04-20 12:22:36 +0100

    Ticketprogram

---
+++ src/main/java/com/example/ticket/Ticket.java
@@ -0,0 +1,29 @@
+package com.example.ticket;
+
+public class Ticket {
+
+        private int ticketNo;
+        private String name;
+        private String destination;
+
+        public Ticket(int ticketNo, String name, String destination) {
+            this.ticketNo = ticketNo;
+            this.name = name;
+            this.destination = destination;
+        }
+
+        public int getTicketNo() {
+            return ticketNo;
+        }
+
+        public String getDestination() {
+            return destination;
+        }
+
+        public String getName() {
+            return name;
+
+        }
+    }
+
+
